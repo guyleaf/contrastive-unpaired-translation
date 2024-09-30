e875b56bd87d54ad0ecd05139a28ec25bd9dfced
Leaf Ying <leaf.ying.work@gmail.com>
2024-09-30 17:43:52 +0800

    Support customizing domain folder and specifying resampling method in unaligned dataset

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -1,9 +1,13 @@
+import argparse
 import os.path
+import random
+from operator import attrgetter
+
+from PIL import Image
+
+import util.util as util
 from data.base_dataset import BaseDataset, get_transform
 from data.image_folder import make_dataset
-from PIL import Image
-import random
-import util.util as util
 
 
 class UnalignedDataset(BaseDataset):
@@ -15,7 +19,42 @@ class UnalignedDataset(BaseDataset):
     You can train the model with the dataset flag '--dataroot /path/to/data'.
     Similarly, you need to prepare two directories:
     '/path/to/data/testA' and '/path/to/data/testB' during test time.
+
+    You can also specify the relative path of domain folder with the domain flag '--data_domainA train/A --data_domainB train/B'.
+    By default, domain folder will be set as the phase + 'A' or 'B'.
     """
+
+    @staticmethod
+    def modify_commandline_options(parser: argparse.ArgumentParser, is_train: bool):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+        """
+        parser.add_argument(
+            "--data_domainA",
+            type=str,
+            default=None,
+            help="relative path of domain A folder to the dataroot",
+        )
+        parser.add_argument(
+            "--data_domainB",
+            type=str,
+            default=None,
+            help="relative path of domain B folder to the dataroot",
+        )
+        parser.add_argument(
+            "--resampling",
+            type=str,
+            default=Image.Resampling.BICUBIC.name,
+            choices=list(map(attrgetter("name"), Image.Resampling)),  # noqa: F821
+            help="relative path of domain B folder to the dataroot",
+        )
+        return parser
 
     def __init__(self, opt):
         """Initialize this dataset class.
@@ -24,16 +63,27 @@ class UnalignedDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
-        self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
 
-        if opt.phase == "test" and not os.path.exists(self.dir_A) \
-           and os.path.exists(os.path.join(opt.dataroot, "valA")):
+        self.dir_A = opt.phase + "A" if opt.data_domainA is None else opt.data_domainA
+        self.dir_B = opt.phase + "B" if opt.data_domainB is None else opt.data_domainB
+
+        self.dir_A = os.path.join(opt.dataroot, self.dir_A)
+        self.dir_B = os.path.join(opt.dataroot, self.dir_B)
+
+        if (
+            opt.phase == "test"
+            and not os.path.exists(self.dir_A)
+            and os.path.exists(os.path.join(opt.dataroot, "valA"))
+        ):
             self.dir_A = os.path.join(opt.dataroot, "valA")
             self.dir_B = os.path.join(opt.dataroot, "valB")
 
-        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
-        self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
+        self.A_paths = sorted(
+            make_dataset(self.dir_A, opt.max_dataset_size)
+        )  # load images from '/path/to/data/trainA'
+        self.B_paths = sorted(
+            make_dataset(self.dir_B, opt.max_dataset_size)
+        )  # load images from '/path/to/data/trainB'
         self.A_size = len(self.A_paths)  # get the size of dataset A
         self.B_size = len(self.B_paths)  # get the size of dataset B
 
@@ -49,25 +99,30 @@ class UnalignedDataset(BaseDataset):
             A_paths (str)    -- image paths
             B_paths (str)    -- image paths
         """
-        A_path = self.A_paths[index % self.A_size]  # make sure index is within then range
-        if self.opt.serial_batches:   # make sure index is within then range
+        A_path = self.A_paths[
+            index % self.A_size
+        ]  # make sure index is within then range
+        if self.opt.serial_batches:  # make sure index is within then range
             index_B = index % self.B_size
-        else:   # randomize the index for domain B to avoid fixed pairs.
+        else:  # randomize the index for domain B to avoid fixed pairs.
             index_B = random.randint(0, self.B_size - 1)
         B_path = self.B_paths[index_B]
-        A_img = Image.open(A_path).convert('RGB')
-        B_img = Image.open(B_path).convert('RGB')
+        A_img = Image.open(A_path).convert("RGB")
+        B_img = Image.open(B_path).convert("RGB")
 
         # Apply image transformation
         # For CUT/FastCUT mode, if in finetuning phase (learning rate is decaying),
         # do not perform resize-crop data augmentation of CycleGAN.
         is_finetuning = self.opt.isTrain and self.current_epoch > self.opt.n_epochs
-        modified_opt = util.copyconf(self.opt, load_size=self.opt.crop_size if is_finetuning else self.opt.load_size)
-        transform = get_transform(modified_opt)
+        modified_opt = util.copyconf(
+            self.opt,
+            load_size=self.opt.crop_size if is_finetuning else self.opt.load_size,
+        )
+        transform = get_transform(modified_opt, method=Image.Resampling[modified_opt.resampling])
         A = transform(A_img)
         B = transform(B_img)
 
-        return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
+        return {"A": A, "B": B, "A_paths": A_path, "B_paths": B_path}
 
     def __len__(self):
         """Return the total number of images in the dataset.
