68353c363ec5eee2bda481e8cf64e3c59b2dcd2a
Leaf Ying <leaf.ying.work@gmail.com>
2024-11-12 14:27:03 +0800

    Merge all test shell scripts to one general script

--- scripts/test_horse2zebra_CUT.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-for epoch in {5..400..5}
-do
-    python -m experiments horse2zebra test 0 \
-                --which_epoch "$epoch" \
-                --gpu_id 5
-done
